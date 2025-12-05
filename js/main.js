48a6ee0adb026d3c0d4363aeee1f74b1f5140272
remydambach <remy.dambach@gadz.org>
2025-12-05 16:43:37 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-fetch('content.json').then(r=>r.json()).then(d=>{
- document.getElementById('hero_title').textContent=d.texts.hero_title;
- document.getElementById('hero_sub').textContent=d.texts.hero_sub;
- const ul=document.getElementById('products');
- d.products_order.forEach(p=>{
-   const li=document.createElement('li'); li.textContent=p; ul.appendChild(li);
- });
-});
